6cc701343fd0326d6830e6c523a2deb249160e2e
Alicukur <alicukur.yzlm@gmail.com>
2022-09-11 17:50:21 +0300

    DomEtkilesimleri

--- JavaScript/DomEtkinliğiCalısmak.js
+++ JavaScript/DomEtkinliğiCalısmak.js
@@ -1,6 +1,6 @@
 // ! ilk bas o etkinliğe ne yapılıcaksa ona ulasmak birinci öncelik daha sonra kompleks yapıları olustyurabşkriz.
 
-let info = document.querySelector("#demo").addEventListener("mouseover" , element)
+// let info = document.querySelector("#demo").addEventListener("mouseover" , element)
 
 // burda etkileşimi yakalamak önemli daha sonra kopleks yapıları olusturabiliriz.
 
@@ -11,4 +11,24 @@ let info = document.querySelector("#demo").addEventListener("mouseover" , elemen
 // function mouseOut(){
 //     document.getElementById('demo').style.color="blue";
 // }
+
+//! form yapılarını kullanarak örnekler yapıyoruz . 
+
+// const color =document.querySelector("#select")
+
+// color.addEventListener("change" , selectbox)
+// function selectbox(event)
+// {
+//     console.log("Etkinliğin tipi :"+ event.type)
+//     console.log("deger : " + event.target.value)
+// }
+
+// const selectBox = document.querySelector('#selectColor');
+// const button = document.querySelector('#button')
+// const form = document.querySelector('#selectColor')
+// const cerceve = document.querySelector('.container')
+// selectBox.addEventListener('click' , function(){
+//     event.stopPropagation(),
+//     console.log("selectBox");
+// })
 
